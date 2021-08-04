ba133c48d33e7670a1fa7484e8e4c85454d15c60
Nicole Tevez <71675833+naniitevez@users.noreply.github.com>
2021-08-04 00:14:51 -0300

    agrego archivos css

--- src/App.js
+++ src/App.js
@@ -1,38 +1,13 @@
 import React, { Component } from 'react'
-
+import Home from './components/Home'
 import './app.css'
-import logo from './logo.png'
+import './styles.css'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <span>
-            Learn{' '}
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              React
-            </a>{' '}
-            and{' '}
-            <a
-              className="App-link"
-              href="https://react-static.js.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              React Static
-            </a>
-          </span>
-        </header>
+      <div>
+        <Home/>
       </div>
     )
   }
